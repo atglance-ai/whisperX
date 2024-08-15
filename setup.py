f0eed47aca040b04bb7fff2139dc14de9fddc9b9
Petter <petter@growbotics.se>
2024-08-15 12:05:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url="https://github.com/m-bain/whisperx",
     license="MIT",
     packages=find_packages(exclude=["tests*"]),
-    install_requires=["pyannote.audio==3.1.1", "faster-whisper==1.0.1"],
+    install_requires=["faster-whisper==1.0.1"],
     entry_points={
         "console_scripts": ["whisperx=whisperx.transcribe:cli"],
     },
